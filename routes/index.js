458b500261fe23050676f0929ea7af12c7867570
Filippo Calzavara <filippo.calzavara@mail.polimi.it>
2018-05-04 17:12:06 +0200

    To dev

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 	res.render('index', {
 		title: 'Dr.Watson',
-		tag_title_description: 'The fake news huter',
+		tag_title_description: 'The Fake News Inspector',
 		description: 'An AI-based platform to check fake news on twitter via Watson IBM'
 	});
 });
