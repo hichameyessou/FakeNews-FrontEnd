0c8cbd827583551439f97441c58367a9631cc890
Filippo Calzavara <filippo.calzavara@mail.polimi.it>
2018-05-07 16:27:06 +0200

    Import from other repo

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,20 @@
 var express = require('express');
+var jsonfile = require('jsonfile')
+var pick = require('random-pick')
+
+
 var router = express.Router();
+var demoTweets = jsonfile.readFileSync('demoTweets.json')
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('index', {
 		title: 'Dr.Watson',
 		tag_title_description: 'The Fake News Inspector',
-		description: 'An AI-based platform to check fake news on twitter via Watson IBM'
+		description: 'An AI-based platform to check fake news on twitter via Watson IBM',
+		demoTweets: pick(demoTweets, 3)
 	});
 });
 
