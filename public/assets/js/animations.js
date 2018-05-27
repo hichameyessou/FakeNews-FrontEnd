e2074586ca7e5f34940a86307f06efaf57ce002c
Filippo Calzavara <filippo.calzavara@mail.polimi.it>
2018-05-27 16:24:02 +0200

    Loader

--- public/assets/js/animations.js
+++ public/assets/js/animations.js
@@ -3,18 +3,9 @@ $(document).ready(function() {
 		$(".tweet0").hide()
 		$(".tweet1").hide()
 		$(".tweet2").animate({ top: '0px', left: '0px' });
-		$(".response").toggleClass('show')
-	});
-	$(".tweet1").click(function() {
-		$(".tweet0").hide()
-		$(".tweet2").hide()
-		$(".tweet1").animate({ top: '0px', left: '0px' });
-		$(".response").toggleClass('show')
-	});
-	$(".tweet0").click(function() {
-		$(".tweet1").hide()
-		$(".tweet2").hide()
-		$(".tweet0").animate({ top: '0px', left: '0px' });
-		$(".response").toggleClass('show')
+		$(".response").toggleClass('show');
+		setTimeout(function() {
+			$(".part").toggleClass('hide');
+		}, 2000);
 	});
 });
