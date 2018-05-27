970365a42c45a1356eee100b16fcc7fbd3877f36
Filippo Calzavara <filippo.calzavara@mail.polimi.it>
2018-05-27 18:04:11 +0200

    BugFix demotweet2

--- public/assets/js/animations.js
+++ public/assets/js/animations.js
@@ -1,19 +1,23 @@
-$(document).ready(function() {
-	$(".tweet2").click(function() {
-		$(".resultLoaded").hide();
-
-		$(".tweet0").toggle()
-		$(".tweet1").toggle()
-		$(".tweet2").toggleClass('positionZero');
-		$(".response").toggle();
-
+var demotweet2 = function(e) {
+	$(".resultLoaded").hide();
+	$(".tweet2").off();
+	$(".tweet0").toggle()
+	$(".tweet1").toggle()
+	$(".tweet2").toggleClass('positionZero');
+	$(".response").toggle();
+	if ($(".response").is(":visible")) {
 		setTimeout(function() {
 			$(".part").toggle();
 			$(".resultLoaded").toggle();
+			$(".tweet2").on('click', demotweet2);
+		}, 3000);
+	} else {
+		$(".part").toggle();
+		$(".resultLoaded").toggle();
+		$(".tweet2").on('click', demotweet2);
 
-		}, 2000);
-
-
-
-	});
+	}
+}
+$(document).ready(function() {
+	$(".tweet2").on("click", demotweet2);
 });
